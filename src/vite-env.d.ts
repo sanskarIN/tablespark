a8e2a3298a02ec600767b11c80ed8ce228ffb4b1
Sanskar <sanskarin@outlook.in>
2026-08-20 06:22:30 +0530

    types: declare cross-platform build constants

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,5 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/client" />
+
+declare const __TABLESPARK_NATIVE__: boolean;
+declare const __TABLESPARK_PLATFORM__: string;
