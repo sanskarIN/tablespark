82ae92d5e2b9c562bfbdc828a00af16c24c65f28
Sanskar <sanskarin@outlook.in>
2026-08-24 13:07:45 +0530

    chore(lint): allow numeric interpolation in typed strings

--- eslint.config.js
+++ eslint.config.js
@@ -44,10 +44,15 @@ export default tseslint.config(
         'error',
         { checksVoidReturn: { attributes: false } },
       ],
+      '@typescript-eslint/restrict-template-expressions': ['error', { allowNumber: true }],
     },
   },
   {
     files: ['**/*.test.{ts,tsx}', 'src/test/**/*.{ts,tsx}'],
     rules: { '@typescript-eslint/no-non-null-assertion': 'off' },
+  },
+  {
+    files: ['src/i18n/LocaleContext.tsx'],
+    rules: { 'react-refresh/only-export-components': 'off' },
   },
 );
